fa0dbd670e30c4f5aa5bdde83a514f02c774f355
Tim Gross <tim@0x74696d.com>
2016-11-29 10:26:11 -0500

    merge demo and setup scripts

--- README.md
+++ README.md
@@ -22,36 +22,35 @@ The Vault health check will check to make sure that we're unsealed and not adver
 
 The operator will then initialize one of the Vault nodes. To do so, the operator provides a PGP public key file for each user and a script will take these files and initialize the vault with `-key-shares` equal to the number of keys provided and `-key-threshold=2` (so that any two of the operators can unseal or force it to be rekeyed -- this is the minimum). The script will then decrypt the operator's own Vault key and use it to unseal all the Vault nodes. The script will also provide the operator with the root key, which will get used to set up ACLs for client applications (see below).
 
-Once the Vaults are unsealed and the Vault is healthy, the Consul service's `onChange` handler will notice and generate a gossip encryption key and add this to the Consul cluster via consul keyring, thereby encrypting all gossip communication between Consul servers.
+Once the Vaults are unsealed and the Vault is healthy, the Consul service's `onChange` handler will notice and generate a gossip encryption key and add this to the Consul cluster via `consul keyring`, thereby encrypting all gossip communication between Consul servers.
 
 ### High Availability
 
 Vault elects a primary via locks in Consul. If the primary fails, a new node will become the primary. Other nodes will automatically redirect via client requests (with an HTTP307) to the primary. If a node is restarted or a new node created it will be sealed and unable to enter the pool until it's manually unsealed.
 
-### Setting up ACLs for an application
-
-TODO
-
-
 ---
 
-## Run it!
+## Run the demo!
+
+This repo provides a tool (`./setup.sh`) to launch and manage the Vault cluster. You'll need the following to get started:
 
 1. [Get a Joyent account](https://my.joyent.com/landing/signup/) and [add your SSH key](https://docs.joyent.com/public-cloud/getting-started).
 1. Install the [Docker Toolbox](https://docs.docker.com/installation/mac/) (including `docker` and `docker-compose`) on your laptop or other environment, as well as the [Joyent Triton CLI](https://www.joyent.com/blog/introducing-the-triton-command-line-tool).
 
-Check that everything is configured correctly by running `./setup.sh`. This will check that your environment is setup correctly and will create an `_env` file that includes injecting an environment variable for a service name for Consul in Triton CNS. We'll use this CNS name to bootstrap the cluster.
+If you want to see how a completed stack looks, try the demo first.
 
-# TODO: need to update this w/ unsealing process
+**`setup.sh demo`:** Runs a demonstration of the entire stack on Triton, creating a 3-node cluster with RPC over TLS. The demo includes initializing the Vault and unsealing it with PGP keys. You can either provide the demo with PGP keys and TLS certificates or allow the script to generate them for you. Parameters:
+
+	-p, --pgp-key        use this PGP key in lieu of creating a new one
+	-k, --tls-key        use this TLS key file in lieu of creating a CA and cert
+	-c, --tls-cert       use this TLS cert file in lieu of creating a CA and cert-
+	-f, --compose-file   use this Docker Compose manifest
+
+**`setup.sh demo clean`:** Cleans up the demo PGP keys and CA.
+
+The Vault cluster runs as Docker containers on Triton, so you can use your Docker client and Compose to explore the cluster further.
 
 ```bash
-$ docker-compose up -d
-Creating vault_vault_1
-
-$ docker-compose scale vault=3
-Creating and starting vault_vault_2 ...
-Creating and starting vault_vault_3 ...
-
 $ docker-compose ps
 Name                      Command                 State       Ports
 --------------------------------------------------------------------------------
@@ -77,7 +76,30 @@ vault_vault_3   /usr/local/bin/containerpilot...   Up   53/tcp, 53/udp,
 $ docker exec -it vault_vault_3 consul info | grep num_peers
     num_peers = 2
 
-$ docker exec -it vault_vault_3 vault???? TODO
-
+$ docker exec -it vault_vault_3 vault write secret/hello value=world
+Success! Data written to: secret/hello
 
 ```
+
+---
+
+## Run it in production!
+
+Once you've seen the demo, you'll want to run the stack as you will in production.
+
+**`setup.sh check`:** Checks that your Triton and Docker environment is sane and configures an environment file `_env` with a CNS record for Consul. We'll use this CNS name to bootstrap the Consul cluster.
+
+**`setup.sh up`:** Starts the Vault cluster via Docker Compose and waits for all instances to be up. Once instances are up, it will poll Consul's status to ensure the raft has been created.
+
+**`setup.sh init`:** Initializes a started Vault cluster. Creates encrypted keyfiles for each operator's public key, which should be redistributed back to operators out-of-band. Use the following options:
+
+	--keys/-k "<val>,<val>":
+		List of public keys used to initialize the vault. These keys
+		must be base64 encoded public keys without ASCII armoring.
+	--threshold/-t <val>:
+		Optional number of keys required to unseal the vault. Defaults
+		to 1 if a single --keys argument was provided, otherwise 2.
+
+**`setup.sh unseal [keyfile]`:** Unseals a Vault with the provided operator's key. Requires access to all Vault nodes via `docker exec`. A number of operator keys equal to the `--threshold` parameter (above) must be used to unseal the Vault.
+
+**`setup.sh policy [policyname] [policyfile]`:** Adds an ACL to the Vault cluster by uploading a policy HCL file and writing it via `vault policy-write`.
--- setup.sh
+++ setup.sh
@@ -6,8 +6,13 @@ cat << EOF
 Usage: ./setup.sh [options] <command>
 --------------------------------------------------------------------------------
 setup.sh check:
-	Checks that your Triton and Docker environment is sane and configures
-	an environment file with a CNS record for Consul.
+	Checks that your Triton and Docker environment is sane and configures an
+	environment file with a CNS record for Consul.
+
+setup.sh up:
+	Starts a 3-node Vault cluster via Docker Compose and waits for all instances
+	to be up. Once instances are up, it will poll Consul's status to ensure the
+	raft has been created.
 
 setup.sh init:
 	Initializes a started Vault cluster. Creates encrypted keyfiles for each
@@ -25,21 +30,61 @@ setup.sh unseal [keyfile]:
 	Unseals a Vault with the provided operator's key. Requires access to all
 	Vault nodes via 'docker exec'. A number of operator keys equal to the
 	'--threshold' parameter (above) must be used to unseal the Vault.
+
+setup.sh policy [policyname] [policyfile]:
+	Adds an ACL to the Vault cluster by uploading a policy HCL file and writing
+	it via 'vault policy-write'.
+
+--------------------------------------------------------------------------------
+
+setup.sh demo:
+	Runs a demonstration of the entire stack on Triton, creating a 3-node
+	cluster with RPC over TLS. The demo includes initializing the Vault and
+	unsealing it with PGP keys. You can either provide the demo with PGP keys
+	and TLS certificates or allow the script to generate them for you.
+	Parameters:
+
+	-p, --pgp-key        use this PGP key in lieu of creating a new one
+	-k, --tls-key        use this TLS key file in lieu of creating a CA and cert
+	-c, --tls-cert       use this TLS cert file in lieu of creating a CA and cert-
+	-f, --compose-file   use this Docker Compose manifest
+
+setup.sh demo clean:
+	Cleans up the demo PGP keys and CA.
+
 EOF
 
 }
+
+# project and service name
+project=vault
+service=consul-vault
+vault="${project}_${service}"
+COMPOSE_FILE=${COMPOSE_FILE:-docker-compose.yml}
+
+# TLS setup paths
+openssl_config=/usr/local/etc/openssl/openssl.cnf
+ca=secrets/CA
+
+# formatting
+fmt_rev=$(tput rev)
+fmt_bold=$(tput bold)
+fmt_reset=$(tput sgr0)
 
 # populated by `check` function whenever we're using Triton
 TRITON_USER=
 TRITON_DC=
 TRITON_ACCOUNT=
 
-
+# prints the argument bold and then resets the terminal colors
+bold() {
+    echo "${fmt_bold}${1}${fmt_reset}"
+}
 # upload public key file to Vault
 _copy_key() {
     local keyfile=$1
     echo "Uploading public keyfile ${keyfile} to vault instance"
-    docker cp secrets/${keyfile} vault_consul-vault_1:${keyfile}
+    docker cp secrets/${keyfile} ${vault}_1:${keyfile}
 }
 
 
@@ -93,7 +138,7 @@ init() {
     do
         _copy_key ${key}
     done
-    docker exec -it vault_consul-vault_1 vault init \
+    docker exec -it ${vault}_1 vault init \
            -address='http://127.0.0.1:8200' \
            -key-shares=${#KEYS[@]} \
            -key-threshold=${THRESHOLD} \
@@ -123,9 +168,25 @@ unseal() {
     echo
     echo 'Use the token above when prompted while we unseal each Vault node...'
     for i in {1..3}; do
-        docker exec -it vault_consul-vault_$i \
+        docker exec -it ${vault}_$i \
              vault unseal -address='http://127.0.0.1:8200'
     done
+}
+
+# upload a local policy file to a Vault instance and write it to the Vault
+# via `docker exec`
+policy() {
+    local policyname=$1
+    local policyfile=$2
+    if [ -z ${policyname} ]; then
+        echo 'You must provide a name for the policy!'; exit 1
+    elif [ ! -f ${policyfile} ]; then
+        echo "${policyfile} not found."; exit 1
+    fi
+
+    docker cp ${policyfile} ${vault}_1:/tmp/${policyfile}
+    docker exec -ti ${vault}_1 \
+           vault policy-write ${policyname} /tmp/${policyfile}
 }
 
 
@@ -189,6 +250,177 @@ check() {
     fi
 }
 
+check_triton() {
+    echo
+    bold '* Checking your setup...'
+    echo './setup.sh check'
+    COMPOSE_FILE=${COMPOSE_FILE} ./setup.sh check
+}
+
+up() {
+    _demo_up
+    _demo_wait_for_consul
+}
+
+_demo_up() {
+    echo
+    bold '* Standing up the Vault cluster...'
+    echo "docker-compose -f ${COMPOSE_FILE} up -d"
+    docker-compose -f "${COMPOSE_FILE}" up -d
+    echo "docker-compose -f ${COMPOSE_FILE} scale ${service}=3"
+    docker-compose -f "${COMPOSE_FILE}" scale "${service}"=3
+}
+
+_demo_wait_for_consul() {
+    echo
+    bold '* Waiting for Consul to form raft...'
+    while :
+    do
+        docker exec -it ${vault}_1 consul info | grep -q "num_peers = 2" && break
+        echo -n '.'
+        sleep 1
+    done
+    reset # something here is breaking the terminal
+}
+
+
+check_tls() {
+    if [ -z "${TLS_CERT}" ] || [ -z "${TLS_KEY}" ]; then
+        cat << EOF
+${fmt_rev}${fmt_bold}You have not provided a value for --tls-cert or --tls-key. In the next step we
+will create a temporary certificate authority in the secrets/ directory and use
+it to issue a TLS certificate. The TLS cert and its key will be uploaded to the
+Vault instances.${fmt_reset}
+EOF
+        echo
+        read -rsp $'Press any key to continue or Ctrl-C to cancel...\n' -n1 key
+        echo
+        _ca
+        _cert
+    fi
+}
+
+
+_ca() {
+    [ -f "${ca}/ca_key.pem" ] && echo 'CA exists' && return
+    [ -f "${ca}/ca_cert.pem" ] && echo 'CA exists' && return
+
+    bold '* Creating a certificate authority...'
+    mkdir -p "${ca}"
+
+    # create a cert we can use to sign other certs (a CA)
+    openssl req -new -x509 -days 3650 -extensions v3_ca \
+            -keyout "${ca}/ca_key.pem" -out "${ca}/ca_cert.pem" \
+            -config "${openssl_config}"
+}
+
+_cert() {
+    [ -f "secrets/consul-vault.key.pem" ] && echo 'TLS certificate exists!' && return
+    [ -f "secrets/consul-vault.csr.pem" ] && echo 'TLS certificate exists!' && return
+    [ -f "secrets/consul-vault.cert.pem" ] && echo 'TLS certificate exists!' && return
+
+    bold '* Creating a private key for Consul and Vault...'
+    openssl genrsa -out "secrets/consul-vault.key.pem" 2048
+
+    bold '* Generating a Certificate Signing Request for Consul and Vault...'
+    openssl req -config ${openssl_config} \
+            -key "secrets/consul-vault.key.pem" \
+            -new -sha256 -out "secrets/consul-vault.csr.pem"
+
+    bold '* Generating a TLS certificate for Consul and Vault...'
+    openssl x509 -req -days 365 -sha256 \
+            -CA "${ca}/ca_cert.pem" \
+            -CAkey "${ca}/ca_key.pem" \
+            -CAcreateserial \
+            -in "secrets/consul-vault.csr.pem" \
+            -out "secrets/consul-vault.cert.pem" \
+
+    bold '* Verifying certificate...'
+    openssl x509 -noout -text \
+            -in "secrets/consul-vault.cert.pem"
+}
+
+
+check_pgp() {
+    if [ -z ${PGP_KEY} ]; then
+        cat << EOF
+${fmt_rev}${fmt_bold}You have not provided a value for --pgp-key. In the next step we will create a
+trusted PGP keypair in your GPG key ring. The public key will be uploaded to the
+Vault instances. The private key will not be exported or leave this machine!${fmt_reset}
+EOF
+        echo
+        read -rsp $'Press any key to continue or Ctrl-C to cancel...\n' -n1 key
+        echo
+        mkdir -p secrets/
+        bold '* Creating PGP key...'
+        gpg -q --batch --gen-key <<EOF
+Key-Type: RSA
+Key-Length: 2048
+Name-Real: Example User
+Name-Email: example@example.com
+Expire-Date: 0
+%commit
+EOF
+        gpg --export 'Example User <example@example.com>' | base64 > secrets/example.asc
+        PGP_KEYFILE="example.asc"
+        bold '* Created a PGP key and exported the public key to ./secrets/example.asc'
+    else
+        bold '* Exporting PGP public key ${PGP_KEY_ARG} to file'
+        gpg --export "${PGP_KEY_ARG}" | base64 > secrets/${PGP_KEY_ARG}.asc
+        PGP_KEYFILE="secrets/${PGP_KEY_ARG}.asc"
+    fi
+}
+
+_demo_init() {
+    echo
+    bold '* Initializing the vault with your PGP key. If you had multiple keys you'
+    bold '  would pass these into the setup script as follows:'
+    echo '  ./setup.sh -k 'mykey1.asc,mykey2.asc' -t 2 init'
+    echo
+    echo "./setup.sh -k ${PGP_KEYFILE} -t 1 init"
+    COMPOSE_FILE=${COMPOSE_FILE} ./setup.sh -k "${PGP_KEYFILE}" -t 1 init
+}
+
+_demo_unseal() {
+    echo
+    bold '* Unsealing the vault with your PGP key. If you had multiple keys,';
+    bold '  each operator would unseal the vault with their own key as follows:'
+    echo '  ./setup.sh unseal secrets/mykey1.asc.key'
+    echo
+    echo "./setup.sh unseal ${PGP_KEYFILE}.key"
+    COMPOSE_FILE=${COMPOSE_FILE} ./setup.sh unseal "secrets/${PGP_KEYFILE}.key"
+}
+
+clean() {
+    bold '* Deleting the key(s) associated with the example user'
+    local key=$(gpg --list-keys 'Example User <example@example.com>' | awk -F'/| +' '/pub/{print $3}')
+    gpg --delete-secret-keys $key
+    gpg --delete-keys $key
+    bold '* Deleting the CA and associated keys'
+    rm -rf secrets/
+}
+
+demo() {
+    while true; do
+        case $1 in
+            -p | --pgp-key ) PGP_KEY=$2; shift 2;;
+            -k | --tls-key ) TLS_KEY=$2; shift 2;;
+            -c | --tls-cert ) TLS_CERT=$2; shift 2;;
+            -f | --compose-file ) COMPOSE_FILE=$2; shift 2;;
+            _ca | check_* | clean | help) cmd=$1; shift 1; $cmd; exit;;
+            *) break;;
+        esac
+    done
+
+    check_tls
+    check_pgp
+    check_triton
+    _demo_up
+    _demo_wait_for_consul
+    _demo_init
+    _demo_unseal
+}
+
 # ---------------------------------------------------
 # parse arguments
 
@@ -196,7 +428,7 @@ while true; do
     case $1 in
         -t | --threshold ) THRESHOLD=$2; shift 2;;
         -k | --keys ) KEYS_ARG=$2; shift 2;;
-        check | init | unseal | help) cmd=$1; shift; break;;
+        check | check_* | up | init | unseal | policy | demo | help) cmd=$1; shift; break;;
         *) break;;
     esac
 done
